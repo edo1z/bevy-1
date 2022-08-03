8699842a946b2106b7514dd939da215f5bd01e20
web3ten0 <web3ten0@gmail.com>
2022-08-04 01:25:33 +0900

    greet people

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,40 @@
 use bevy::prelude::*;
 
-fn main() {
-    App::new().add_system(hello_world).run();
+#[derive(Component)]
+struct Person;
+
+#[derive(Component)]
+struct Name(String);
+
+fn add_people(mut commands: Commands) {
+    commands
+        .spawn()
+        .insert(Person)
+        .insert(Name("Taro".to_string()));
+    commands
+        .spawn()
+        .insert(Person)
+        .insert(Name("Jiro".to_string()));
+    commands
+        .spawn()
+        .insert(Person)
+        .insert(Name("Saburo".to_string()));
 }
 
 fn hello_world() {
     println!("hello world!");
+}
+
+fn greet_people(query: Query<&Name, With<Person>>) {
+    for name in query.iter() {
+        println!("hello {}", name.0);
+    }
+}
+
+fn main() {
+    App::new()
+        .add_startup_system(add_people)
+        .add_system(hello_world)
+        .add_system(greet_people)
+        .run();
 }
